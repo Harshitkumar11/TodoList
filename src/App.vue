7762bf036b45ecaee99925267cc8f0c9de8eb2a4
HARSHIT KUMAR <harshitkumar1032@gmail.com>
2024-03-14 00:26:28 +0530

    todo_list using vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id ="app">
+    <Todo-List></Todo-List>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+
